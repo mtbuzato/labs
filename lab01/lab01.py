bc316258caadd4a51ab58ad132d93e5d2c6064a2
Miguel Buzato <miguel@mtbuzato.com.br>
2020-04-18 01:13:50 -0300

    Adequado lab01 com feedback
    Foram alterados os nomes de algumas variáveis para deixar sua função mais clara

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -10,11 +10,11 @@
 from decimal import Decimal
 
 if __name__ == "__main__":
-    inp = Decimal(input())                                                                  # lê o valor na entrada e transforma em decimal
-    inpInt = int(inp)                                                                       # cria uma cópia do valor da entrada arrendondada
-    dezena = int(inp // 10)                                                                 # divide o valor da entrada arredondada por 10, arredondado, calculando a dezena do número
-    unidade = int(inp - dezena * 10)                                                        # subtrai a dezena do número para ficar apenas com as unidades
-    decimais = int((inp - inpInt) * 100)                                                    # transforma os decimais em um número inteiro (12.34 --> 34)
+    entrada = Decimal(input())                                                              # lê o valor na entrada e transforma em decimal
+    entradaArredondada = int(entrada)                                                       # cria uma cópia do valor da entrada arrendondada
+    dezena = int(entrada // 10)                                                             # divide o valor da entrada arredondada por 10, arredondado, calculando a dezena do número
+    unidade = int(entrada - dezena * 10)                                                    # subtrai a dezena do número para ficar apenas com as unidades
+    decimais = int((entrada - entradaArredondada) * 100)                                    # transforma os decimais em um número inteiro (12.34 --> 34)
     decimo = int(decimais // 10)                                                            # divide o valor por 10, arredondado, calculando a dezena do número
     centesimo = int(decimais - decimo * 10)                                                 # subtrai a dezena do numero para ficar apenas com as unidades
     print("R$ " + str(centesimo) + str(decimo) + "." + str(unidade) + str(dezena))          # cria o número invertido e imprime na saída
