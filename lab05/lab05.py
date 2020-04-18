2a4e7e62be7919b8fe80a3f8c64025764f14daee
Miguel Buzato <miguel@mtbuzato.com.br>
2020-04-18 01:14:28 -0300

    Adequado o lab05
    Como não pode fazer o uso de "^" como XOR, foi substituído por uma comparação. Explicação foi propriamente documentada.

--- lab05/lab05.py
+++ lab05/lab05.py
@@ -20,10 +20,12 @@ if __name__ == "__main__":
         if final[i]:                                                    # se o valor lido na passagem atual for 1 (que é quivalente a True), caso contrario não faz nada
             for x in range(polySize):                                       # itera x no tamanho do polinomio
                 if i + x < finalSize:                                           # verifica se ainda há valor na passagem atual para ser XOR, para não dar erro caso esteja próximo do fim
-                    final[i + x] = final[i+x] ^ poly[x]                             # executa o XOR no (i+x)-ésimo da passagem atual com o (x)-ésimo do polinômio, (i+x) para relativizar a passagem atual com o polinomio
+                    final[i + x] = 1 if final[i+x] != poly[x] else 0                             # executa o "XOR"(neste caso o !=) no (i+x)-ésimo da passagem atual com o (x)-ésimo do polinômio, (i+x) para relativizar a passagem atual com o polinomio*
 
     # print("".join([str(x) for x in final])[-crcSize:])            # método mais simples para imprimir o valor final, porém utiliza de coisas não cobertas no curso ainda
     crc = ""                                                        # variável que será preenchido o CRC
     for i in range(finalSize - 1, finalSize - 1 - crcSize, -1):     # itera i, começando no último valor da array (final) ate o último valor que faz parte do CRC da array (final), com steps de -1, ou seja, andando para trás
         crc = str(final[i]) + crc                                       # coloca o valor no CRC, de trás pra frente
     print(crc)                                                      # imprime o CRC
+
+#*como o código é formado por 1 e 0, o XOR age como uma comparação, visto que ele só prevalece o 1 caso ambos forem diferentes.
